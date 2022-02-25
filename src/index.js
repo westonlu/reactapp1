fb5037c8b3408c3dd48e5bca7b72a32ccf6e73f0
westonlu <westonlu@utexas.edu>
2022-02-25 11:24:02 -0600

    change player text

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Current player: X';
   
       return (
         <div>
